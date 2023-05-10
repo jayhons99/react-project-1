377b46615537ce190d8beb74cef95bfd3fd56c35
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-10 16:16:43 -0400

    finish styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 function Main() {
   return (
-    <div>
-      <h1>Main content goes here</h1>
+    <div className="main-container">
+      <h1 className="main-header">Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   );
 }
